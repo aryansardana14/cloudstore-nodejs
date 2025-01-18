3300e6a5aee7d1222959aa3775ef60156a58a1ee
Aryan Sardana <aryansardana1@gmail.com>
2025-01-18 09:20:44 +0530

    prod

--- config/passportConfig.js
+++ config/passportConfig.js
@@ -42,7 +42,7 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: 'http://localhost:3000/auth/google/api/files',
+            callbackURL: 'http://cloudstore-nodejs-w4np.vercel.app/auth/google/api/files',
             userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo',
         },
         async (accessToken, refreshToken, profile, cb) => {
