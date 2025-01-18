fa7676a520ddcd284d5b7f0566000fd24cd8c75b
Aryan Sardana <aryansardana1@gmail.com>
2025-01-18 08:59:56 +0530

    prod

--- app.js
+++ app.js
@@ -37,7 +37,8 @@ app.use('/auth', authRoutes);
 app.get("/",(req,res)=>{
     res.redirect('/api/files')
 })
-const PORT = process.env.PORT || 3000;
+
+const PORT = process.env.APP_PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
